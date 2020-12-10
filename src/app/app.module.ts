cc41fc86397a143c2ac3cc9a3557dabfab242c0e
JacobViertel <jacobviertel@gmx.de>
2020-12-10 10:31:03 +0100

    finish tut7

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { HighlightPipe } from './highlight.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HighlightPipe
   ],
   imports: [
     BrowserModule,
