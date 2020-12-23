6172295f472fb97a1b91ecbc3746d1d90d507df8
JacobViertel <jacobviertel@gmx.de>
2020-12-23 15:28:52 +0100

    Derviates tutorial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,15 @@ import { LoginComponent } from './login/login.component';
 import { HighlightPipe } from './highlight.pipe';
 import { TodoModule } from './todo/todo.module';
 import { HoverEffectPipe } from './hover-effect.pipe';
+import { TasksComponent } from './tasks/tasks.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     HighlightPipe,
-    HoverEffectPipe
+    HoverEffectPipe,
+    TasksComponent
   ],
   imports: [
     BrowserModule,
